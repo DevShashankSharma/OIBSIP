9f01bd9dec300efb5d29cecad8589c776b62373d
DevShashankSharma <editorshashank429@gmail.com>
2024-01-19 18:11:32 +0530

    Creating Modals for Preview and Edit Todo

--- TASK 6 TO-DO-WEBAPP/script.js
+++ TASK 6 TO-DO-WEBAPP/script.js
@@ -3,6 +3,8 @@ const description = document.getElementById("description");
 let ul = document.querySelector(".pending-task");
 let Cul = document.querySelector(".completed-task");
 
+let edit = 0;
+
 const addTask = () => {
   const task = {
     title: title.value,
@@ -22,18 +24,17 @@ const addTask = () => {
   arr.push(task); //push your new elements
   localStorage.setItem("tasks", JSON.stringify(arr));
   createList(task);
-  console.log(task);
 };
 
 const createList = (task) => {
   let li = `
   <li>
-  <div class="defination">
+  <div onclick="showModal(this)" class="defination">
     <div class="todo-title">${task.title}</div>
     <div class="todo-desc">${task.description}</div>
   </div>
   <div class="btn">
-    <img class="edit" src="svg/edit.svg" alt="" />
+    <img onclick='openModal(this)' class="edit" src="svg/edit.svg" alt="" />
     <img onclick='deleteTask(this,"tasks")' class="delete" src="svg/delete.svg" alt="" />
     <img onclick="CompleteTask(this)" class="mark" src="svg/mark.svg" alt="" />
   </div>
@@ -46,7 +47,7 @@ const createList = (task) => {
 const createCompleteList = (task) => {
   let li = `
     <li>
-      <div class="defination">
+      <div onclick="showModal(this)" class="defination">
         <div class="todo-title">${task.title}</div>
         <div class="todo-desc">${task.description}</div>
       </div>
@@ -78,20 +79,20 @@ const creatCompleteTaskList = () => {
   }
 };
 
-const deleteTask = (e,list) => {
-  let li = e.parentNode.parentNode; 
-  let key = li.firstElementChild.firstElementChild.textContent; 
+const deleteTask = (e, list) => {
+  let li = e.parentNode.parentNode;
+  let key = li.firstElementChild.firstElementChild.textContent;
   let arr = JSON.parse(localStorage.getItem(list));
-  console.log(arr)
+  // console.log(arr);
   for (let i = 0; i < arr.length; i++) {
     if (arr[i].title === key) {
       arr.splice(i, 1);
       localStorage.setItem(list, JSON.stringify(arr));
-      if(list === "tasks"){
+      if (list === "tasks") {
         ul.removeChild(li);
-      }else{
+      } else {
         Cul.removeChild(li);
-      } 
+      }
       break;
     }
   }
@@ -114,8 +115,84 @@ const CompleteTask = (e) => {
   arr.push(task); //push your new elements
   localStorage.setItem("Ctasks", JSON.stringify(arr));
   createCompleteList(task);
-  deleteTask(e,"tasks");
+  deleteTask(e, "tasks");
 };
 
 creatPendingTaskList();
 creatCompleteTaskList();
+
+// Function to open the modal
+const openModal = (e) => {
+  let modal = document.getElementById("modalOverlay");
+  modal.style.display = "flex";
+  let li = e.parentNode.parentNode;
+  let task = {
+    title: li.firstElementChild.firstElementChild.textContent,
+    description: li.firstElementChild.children[1].textContent,
+  };
+
+  let arr = JSON.parse(localStorage.getItem("tasks"));
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].title === task.title) {
+      edit = i;
+      // console.log(edit);
+      break;
+    }
+  }
+  let modal_text = modal.childNodes[1].childNodes[1];
+  modal_text.childNodes[1].value = task.title;
+  modal_text.childNodes[3].value = task.description;
+};
+
+// Function to show the modal
+const showModal = (e) => {
+  let modal = document.getElementById("modalOverlay2");
+  modal.style.display = "flex";
+  let li = e.parentNode;
+  let task = {
+    title: li.firstElementChild.firstElementChild.textContent,
+    description: li.firstElementChild.children[1].textContent,
+  };
+  let modal_text = modal.childNodes[1].childNodes[1];
+  modal_text.childNodes[1].value = task.title;
+  modal_text.childNodes[3].value = task.description;
+};
+
+// Function to close the modal
+const closeModal = () => {
+  document.getElementById("modalOverlay").style.display = "none";
+  document.getElementById("modalOverlay2").style.display = "none";
+};
+
+// Function to save changes and close the modal
+const saveAndClose = (e) => {
+  let modal = e.parentNode;
+  let data = {
+    title: modal.childNodes[1].value,
+    description: modal.childNodes[3].value,
+  };
+
+  let arr = JSON.parse(localStorage.getItem("tasks"));
+  for (let i = 0; i < arr.length; i++) {
+    if (i === edit) {
+      arr[i].title = data.title;
+      arr[i].description = data.description;
+      localStorage.setItem("tasks", JSON.stringify(arr));
+      //reload
+      location.reload();
+      closeModal();
+      break;
+    }
+  }
+  closeModal();
+};
+
+// Close the modal if the overlay is clicked
+window.onclick = function (event) {
+  let modal = document.getElementById("modalOverlay");
+  let modal2 = document.getElementById("modalOverlay2");
+  if (event.target === modal || event.target === modal2) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+  }
+};
